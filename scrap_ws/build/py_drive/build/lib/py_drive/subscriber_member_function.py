53a027c1e41ab1f765bec620f7ff5cce63dccb3d
Ca--rod <kingsta@protonmail.com>
2021-04-30 02:15:25 +0200

    U

--- scrap_ws/build/py_drive/build/lib/py_drive/subscriber_member_function.py
+++ scrap_ws/build/py_drive/build/lib/py_drive/subscriber_member_function.py
@@ -21,6 +21,7 @@ from std_msgs.msg import Int32MultiArray, Int16
 import time
 from adafruit_motorkit import MotorKit
 
+
 # https://github.com/adafruit/Adafruit_CircuitPython_MotorKit.git
 
 
@@ -85,7 +86,7 @@ class Robot(Node):
         # currentSpeedL = -map(dY - configuration.steerGain * dX, -100, 100, -configuration.maxSpeed, configuration.maxSpeed);
         # currentSpeedR = -map(dY + configuration.steerGain * dX, -100, 100,  -configuration.maxSpeed, configuration.maxSpeed);
         speedLimit = 0.3
-        steerGain = 0.5
+        steerGain = 0.3
         speedL = max(
             -speedLimit, min(speedLimit, float(speed[1]) + steerGain * float(speed[0]))
         )
@@ -94,6 +95,32 @@ class Robot(Node):
         )
         self.kit.motor1.throttle = speedL
         self.kit.motor2.throttle = -speedR
+
+    def get_ip_address(interface):
+        state = get_network_interface_state(interface)
+        if state == "down" or state == None:
+            return None
+
+        cmd = (
+            "ifconfig %s | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'"
+            % interface
+        )
+        return subprocess.check_output(cmd, shell=True).decode("ascii")[:-1]
+
+    def get_network_interface_state(interface):
+        if not os.path.exists("/sys/class/net/%s/operstate" % interface):
+            # print("%s file does NOT exist" % interface)
+            return None
+
+        try:
+            status = subprocess.check_output(
+                "cat /sys/class/net/%s/operstate" % interface, shell=True
+            ).decode("ascii")[:-1]
+        except Exception as err:
+            print("Exception: {0}".format(err))
+            return None
+        else:
+            return status
 
 
 def main(args=None):
