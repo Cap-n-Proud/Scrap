d77f0e76a9331896416e0c1367cfa940c5a4321c
Cap-n-Proud <kingsta@protonmail.com>
2021-05-02 11:36:15 +0200

    U

--- utils/start_robot.sh
+++ utils/start_robot.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 sudo mount 192.168.1.121:/mnt/VDev/Software Software
-sudo chmod 777 /dev/i2c-1
-node /home/robot/Scrap/scrap_ws/src/ros2-web-bridge/bin/rosbridge.js &
-node /home/robot/Scrap/web/index.js &
-python3 /home/robot/Scrap/web/mjpeg_http/mjpeg_http/server.py --bind 192.168.1.164 &
+# sudo chmod 777 /dev/i2c-1
+# node /home/robot/Scrap/scrap_ws/src/ros2-web-bridge/bin/rosbridge.js &
+# node /home/robot/Scrap/web/index.js &
+# python3 /home/robot/Scrap/web/mjpeg_http/mjpeg_http/server.py --bind 192.168.1.164 &
 # python3 /home/robot/Scrap/apps/stats.py &
 
 cd /home/robot/Software/ROS2/Scrap/scrap_ws
