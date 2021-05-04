dc7911f342521d376a15e8837f093564bb875e87
Cap-n-Proud <kingsta@protonmail.com>
2021-05-04 22:37:12 +0200

    U

--- web/index.js
+++ web/index.js
@@ -15,9 +15,13 @@
 // 'use strict';
 
 const express = require('express');
+
 const app = express();
 
-app.use(express.static('.'));
+app.use(express.static('html'));
+app.get('/', (req, res) => {
+  res.send('Successful response.');
+});
 
-app.listen(3000);
-console.log('The web server started on http://localhost:3000');
+
+app.listen(3000, () => console.log('Example app is listening on port 3000.'));
