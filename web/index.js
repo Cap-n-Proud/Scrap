2c497ed177e8692b509511f73bc73bc470f99299
Cap-n-Proud <kingsta@protonmail.com>
2021-05-04 22:50:12 +0200

    U

--- web/index.js
+++ web/index.js
@@ -1,27 +1,13 @@
-// Copyright (c) 2017 Intel Corporation. All rights reserved.
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// 'use strict';
-
 const express = require('express');
+const path = require('path');
 
 const app = express();
+const port = process.env.PORT || 8080;
 
-app.use(express.static('html'));
-app.get('/', (req, res) => {
-  res.send('Successful response.');
+// sendFile will go here
+app.get('/', function(req, res) {
+res.sendFile(path.join(__dirname, '/html/vjoy.html'));
 });
 
-
-app.listen(3000, () => console.log('Example app is listening on port 3000.'));
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
