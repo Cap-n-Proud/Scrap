738a9da67552bf3a2cc9d369ef98e2f11aeb3308
Cap-n-Proud <kingsta@protonmail.com>
2021-05-13 16:29:46 +0200

    U

--- utils/start_robot_local.sh
+++ utils/start_robot_local.sh
@@ -5,13 +5,8 @@ sudo chmod 777 /dev/i2c-1
 cd /home/robot/Software/ROS2/Madox/madox_ws
 source /opt/ros/foxy/setup.bash
 . install/local_setup.bash
+
 ros2 run jetbot_drive ros2_jetbot_drive &
-cd /home/robot/Software/ROS2/Madox/madox_ws
-. install/local_setup.bash
 ros2 run jetbot_camera ros2_jetbot_camera &
-cd /home/robot/Software/ROS2/Madox/madox_ws
-. install/local_setup.bash
 ros2 run jetbot_oled_display ros2_jetbot_oled_display &
-cd /home/robot/Software/ROS2/Madox/madox_ws
-. install/local_setup.bash
 ros2 run jetbot_sys_info ros2_jetbot_sys_info &
