795ad1a06810711008a620a97d0a920c3c2159f8
Cap-n-Proud <kingsta@protonmail.com>
2021-06-06 01:22:41 +0200

    Some progress with the imu package, still along way...

--- madox_ws/src/jetbot_camera/setup.py
+++ madox_ws/src/jetbot_camera/setup.py
@@ -1,22 +1,23 @@
 from setuptools import setup
 
-package_name = 'jetbot_camera'
+package_name = "jetbot_camera"
 
 setup(
     name=package_name,
-    version='1.0.0',
+    version="1.0.0",
     packages=[package_name],
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
+        ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
+        ("share/" + package_name, ["package.xml"]),
     ],
-    install_requires=['setuptools'],
+    install_requires=["setuptools"],
     zip_safe=True,
     maintainer="Cap-n-Proud",
-    maintainer_email='robot@todo.todo',
-    description='Manages the camera including overlay information and streaming to web browser via gstreamer.',
+    maintainer_email="robot@todo.todo",
+    description="Manages the camera including overlay information and streaming to web browser via gstreamer.",
     license="Apache 2.0",
-    tests_require=['pytest'],
-    entry_points={"console_scripts": ["ros2_jetbot_camera = jetbot_camera.server:main"]},
+    tests_require=["pytest"],
+    entry_points={
+        "console_scripts": ["ros2_jetbot_camera = jetbot_camera.server:main"]
+    },
 )
